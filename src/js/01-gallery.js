0b793604c7801421bc2809d72f6d433b101430b6
Anton Kovsh <kovsh_anton@ukr.net>
2022-10-14 13:23:57 +0300

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,38 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// // Описан в документации
+// import SimpleLightbox from 'simplelightbox';
+// // Дополнительный импорт стилей
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryMainDiv = document.querySelector('.gallery');
+// console.log(galleryMainDiv);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+// console.log(galleryMarkup);
+
+galleryMainDiv.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryMainDiv.addEventListener('click', onPictureClick);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+function onPictureClick(event) {
+  event.preventDefault();
+  // console.log(event.target);
+  if (event.target.tagName !== 'IMG') return;
+  console.log(event);
+}
+
+// var lightbox = new SimpleLightbox('.gallery__item', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
