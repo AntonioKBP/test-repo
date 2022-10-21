fb267d6247a25be70e35691cbb2ae4948e7df272
Anton Kovsh <kovsh_anton@ukr.net>
2022-10-21 17:31:48 +0300

    upd

---
+++ src/js/expand.js
@@ -0,0 +1,23 @@
+// const divs = document.querySelectorAll('div');
+// console.log(divs);
+
+// // divs.map(div => {
+// //   div.style.backgroundColor = getRandomHexColor();
+// // });
+
+// divsArr = divs.map();
+// console.log(divsArr);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const cards = document.querySelectorAll('.card');
+
+for (let i = 0; i < cards.length; i += 1) {
+  cards[i].style.backgroundColor = getRandomHexColor();
+  cards[i].addEventListener('click', () => {
+    cards[i].classList.add('active');
+  });
+}
--- src/js/training-border-radius-slider.js
+++ src/js/training-border-radius-slider.js
@@ -1,3 +1,8 @@
 const input = document.querySelector('.input');
 const result = document.querySelector('.result');
 const outputBox = document.querySelector('.output');
+
+input.addEventListener('input', () => {
+  result.innerHTML = input.value + '%';
+  outputBox.style.borderRadius = input.value + '%';
+});
