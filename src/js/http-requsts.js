12ab24ab295f054c02f08878a35058f3ce0027a8
Anton Kovsh <kovsh_anton@ukr.net>
2022-11-08 20:45:47 +0200

    upd

--- src/js/http-requsts.js
+++ src/js/http-requsts.js
@@ -4,38 +4,28 @@ fetch('https://pokeapi.co/api/v2/pokemon/2')
   })
   .then(pokemon => {
     console.log(pokemon);
+    const markup = pokemonCardMarkup(pokemon);
+
+    console.log(markup);
   })
   .catch(error => console.log(error));
 
-//   <div class="card">
-//   <div class="card-img-top">
-//     <img src="{{sprites.front_default}}" alt="{{name}}">
-//   </div>
-//   <div class="card-body">
-//     <h2 class="card-title">Имя: {{name}}</h2>
-//     <p class="card-text">Вес: {{weight}}</p>
-//     <p class="card-text">Рост: {{height}}</p>
-
-//     <p class="card-text"><b>Умения</b></p>
-//     <ul class="list-group"></ul>
-//     {{#each abilities}}
-//       <li class="list-group-item">{{ability.name}}</li>
-//     {{/each}}
-//     </ul>
-//   </div>
-// </div>
-
 function pokemonCardMarkup(pokemon) {
-    const markupCard = 
-  return markupCard;
-}
+  return ` <div class="card">
+  <div class="card-img-top">
+    <img src="${sprites.front_default}" alt="${name}">
+  </div>
+  <div class="card-body">
+    <h2 class="card-title">Имя: ${name}</h2>
+    <p class="card-text">Вес: ${weight}</p>
+    <p class="card-text">Рост: ${height}</p>
 
-function createGalleryMarkup(gallery) {
-  return gallery
-    .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
-<img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
-    })
-    .join('');
+    <p class="card-text"><b>Умения</b></p>
+    <ul class="list-group">${abilities}</ul>
+    
+      <li class="list-group-item">${ability.name}</li>
+    
+    </ul>
+  </div>
+</div>`;
 }
